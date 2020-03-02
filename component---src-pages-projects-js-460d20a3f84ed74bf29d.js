5a25c41b5ba92f41623ccca89e30e084fac86a54
Quintin <qorsmond@gmail.com>
2020-03-02 21:52:15 +0200

    Updates

---
+++ component---src-pages-projects-js-460d20a3f84ed74bf29d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"16l3":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"My Projects"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-projects-js-460d20a3f84ed74bf29d.js.map
